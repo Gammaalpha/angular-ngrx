e28d8445495bfa16e9c6d5d0452cb96ef0955b97
Faiq Malik <faiqmalik2@gmail.com>
2021-10-07 14:41:45 -0400

    Update navbar.component.ts

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  headerTitle: string = "IBM Front End Developer Assignment";
+  headerTitle: string = "Angular Ngrx Project";
   constructor() { }
 
   ngOnInit(): void {
